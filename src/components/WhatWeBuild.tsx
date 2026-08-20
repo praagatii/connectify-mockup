7f8297b3109dff2dddc01d82a889acd8df8f1d80
praagatii <praagatii@users.noreply.github.com>
2026-08-20 22:32:38 +0530

    Cut list off cleanly at Our Services heading edge

--- src/components/WhatWeBuild.tsx
+++ src/components/WhatWeBuild.tsx
@@ -52,9 +52,9 @@ export default function WhatWeBuild() {
               className="divide-y divide-black/10 border-y border-black/10"
               style={{
                 maskImage:
-                  "linear-gradient(to bottom, transparent 0, black 1rem)",
+                  "linear-gradient(to bottom, transparent 0, transparent 0.5rem, black 0.5rem)",
                 WebkitMaskImage:
-                  "linear-gradient(to bottom, transparent 0, black 1rem)",
+                  "linear-gradient(to bottom, transparent 0, transparent 0.5rem, black 0.5rem)",
               }}
             >
               {capabilities.map((item) => (

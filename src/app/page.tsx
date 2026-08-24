5222a44531369c56c475e3386df3796be552f803
praagatii <praagatii@users.noreply.github.com>
2026-08-24 15:24:14 +0530

    Move TrustedBy above Projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,8 +10,8 @@ export default function Home() {
     <>
       <Hero />
       <WhatWeBuild />
-      <SelectedWork />
       <TrustedBy />
+      <SelectedWork />
       <Industries />
       <FinalCta />
     </>

47d8a7f9fdbeb91f3b3ddc065e5570f7233078f6
praagatii <praagatii@users.noreply.github.com>
2026-08-27 13:37:24 +0530

    Navbar: barely visible translucent bg

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,7 +15,7 @@ export default function Navbar() {
   const [open, setOpen] = useState(false);
 
   return (
-    <header className="fixed inset-x-0 top-0 z-50">
+    <header className="fixed inset-x-0 top-0 z-50 bg-black/5 backdrop-blur-[2px]">
       <nav className="mx-auto flex h-20 w-full max-w-7xl items-center justify-between px-6 sm:h-24 lg:px-12">
         <Link href="/" className="flex items-center" aria-label="ConnectifyTech">
           {/* eslint-disable-next-line @next/next/no-img-element */}
